5d6c786f4e1c7bba2fae7de592729371efb2b719
djimenezchocron <djimenezchocron@hotmail.com>
2022-11-09 21:23:37 +0100

    Práctica 7 terminada

---
+++ dist/js/scripts.js
@@ -0,0 +1,17 @@
+const menuIconElement = document.getElementById('menu-icon');
+const menuElement = document.getElementById('menu');
+
+menuIconElement.addEventListener('click', (event) => {
+
+    if (event.target.dataset.icon === 'hamburger') {
+        menuIconElement.src = './assets/images/icon-close.svg';
+        menuIconElement.dataset.icon = 'close';
+        menuElement.classList.add('menu--show');
+    }
+    else {
+        menuIconElement.src = './assets/images/icon-hamburger.svg';
+        menuIconElement.dataset.icon = 'hamburger';
+        menuElement.classList.remove('menu--show');
+    }
+}
+)
